c79449f1f3d33c047fea0e048fb922e1183b5c7e
vivek bharda <vivekkbharda@gmail.com>
2019-02-21 18:01:54 +0530

    day44

---
+++ js/style.js
@@ -0,0 +1,9 @@
+mdc.switchControl.MDCSwitch.attachTo(document.querySelector('.switch-1'));
+mdc.switchControl.MDCSwitch.attachTo(document.querySelector('.switch-2'));
+mdc.textField.MDCTextField.attachTo(document.querySelector('.name'));
+mdc.textField.MDCTextField.attachTo(document.querySelector('.address'));
+mdc.textField.MDCTextField.attachTo(document.querySelector('.country'));
+
+
+
+
